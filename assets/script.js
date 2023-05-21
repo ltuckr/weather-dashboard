9e70f665ab257f8a69b32c98376a1d2eadcd9f93
ltuckr <l.tucker.rdh@gmail.com>
2023-05-21 17:37:35 -0400

    call weather function with AJAX

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
-//5 day forecast with my API key
+//My API key
 var myAPI= "496d9caed19b5898e62a2e3928f9c3d3";
 
 //GIVEN a weather dashboard with form inputs
 //WHEN I search for a city
 
-//variables for search: 
+//variables for search action: 
 var searchLocation = $("#search-location");
 var currentLocation = $("#current-location");
 
@@ -15,8 +15,19 @@ var clearHistory = $("#clear-history");
 //variable to commit search results to local storage
 var city="";
 
+//WHEN I view current weather conditions for that city
+//THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+
 //variables for current weather conditions
 var condNow = $("cond-now");
 var tempNow = $("temp-now");
 var humidityNow = $("humidity-now");
 var windNow = $("wind=now");
+
+// Use AJAX to call current weather
+function currentWeather(city){
+    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    $.ajax({
+        url:queryURL,
+        method:"GET",
+    }).then(function(response){
