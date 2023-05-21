0fc9995d681d2f0023d45be1c1596b222c19f70c
ltuckr <l.tucker.rdh@gmail.com>
2023-05-21 16:46:31 -0400

    adding search variables

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,16 @@
 //5 day forecast with my API key
-api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={496d9caed19b5898e62a2e3928f9c3d3}
+var myAPI= "496d9caed19b5898e62a2e3928f9c3d3";
+
+//GIVEN a weather dashboard with form inputs
+//WHEN I search for a city
+
+//variables for search: 
+var searchLocation = $("#search-location");
+var currentLocation = $("#current-location");
+
+//variables for button clicks to add/clear search results:
+var searchBar = ("#search-bar");
+var clearIt = ("#clear-it");
+
+//variable to commit search results to local storage
+var city="";
