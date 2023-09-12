c098e11a161db0ad1cfe19a806367045eaeab6b9
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-09-12 17:49:57 -0400

    corrected repeating text

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-// my API key for openweathermap.org
+// My API key for openweathermap.org
 var apiKey = '496d9caed19b5898e62a2e3928f9c3d3';
 
 // DOM elements
@@ -14,7 +14,6 @@ var fDate0 = document.getElementById('fDate0');
 var fTemp0 = document.getElementById('fTemp0');
 var fHumidity0 = document.getElementById('fHumidity0');
 var fWind0 = document.getElementById('fWind0');
-var fUV0 = document.getElementById('fUV0');
 
 // Event listener for the search button
 searchButton.addEventListener('click', function (event) {
@@ -61,39 +60,12 @@ function displayWeather(data) {
     
     locationElement.textContent = cityName;
     weatherDescriptionElement.textContent = 'Weather Description: ' + data.weather[0].description;
-    fTemp0.textContent = 'Temperature: ' + temperature;
-    fHumidity0.textContent = 'Humidity: ' + humidity;
-    fWind0.textContent = 'Wind Speed: ' + windSpeed;
+    fTemp0.textContent = 'Temperature: ' + k2f(temperature) + ' °F';
+    fHumidity0.textContent = 'Humidity: ' + humidity + '%';
+    fWind0.textContent = 'Wind Speed: ' + windSpeed + ' MPH';
     
     // Call a function to fetch and display the 5-day forecast
     fetchFiveDayForecast(cityName);
-}
-
-// Helper function to format date
-function formatDate(date) {
-    const options = { month: 'numeric', day: 'numeric', year: 'numeric' };
-    return date.toLocaleDateString(undefined, options);
-}
-
-// Function to get UV index based on lat and lon
-function getUVIndex(lat, lon) {
-    var uvIndexUrl = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-    
-    return fetch(uvIndexUrl)
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error('UV Index not available');
-            }
-        })
-        .then(function (data) {
-            return data.value;
-        })
-        .catch(function (error) {
-            console.error('Error:', error);
-            return 'UV Index N/A';
-        });
 }
 
 // Function to display the 5-day forecast
@@ -119,23 +91,25 @@ function fetchFiveDayForecast(cityName) {
 
 // Function to display the 5-day forecast
 function displayFiveDayForecast(data) {
-    
+
     for (let i = 0; i < 5; i++) {
-        const forecastDate = new Date(data.list[i * 8].dt * 1000);
+        const forecastData = data.list[i * 8]; // Get the forecast data for each day
+        const forecastDate = new Date(forecastData.dt * 1000);
         const dateElement = document.getElementById(`fDate${i}`);
         const tempElement = document.getElementById(`fTemp${i}`);
         const humidityElement = document.getElementById(`fHumidity${i}`);
         const windElement = document.getElementById(`fWind${i}`);
-        const uvElement = document.getElementById(`fUV${i}`);
 
         // Set the innerHTML of each card with forecast data
-        dateElement.textContent = formatDate(forecastDate);
-        tempElement.textContent = 'Temperature: ' + k2f(data.list[i * 8].main.temp);
-        humidityElement.textContent = 'Humidity: ' + data.list[i * 8].main.humidity + '%';
-        windElement.textContent = 'Wind Speed: ' + data.list[i * 8].wind.speed + ' MPH';
-        uvElement.textContent = 'UV Index: ' + getUVIndex(data.city.coord.lat, data.city.coord.lon);
+        dateElement.innerHTML = formatDate(forecastDate);
+        tempElement.innerHTML = k2f(forecastData.main.temp) + ' &#176F';
+        humidityElement.innerHTML = forecastData.main.humidity + '%';
+        windElement.innerHTML = forecastData.wind.speed + ' MPH';
     }
 }
 
-
-
+// Helper function to format date
+function formatDate(date) {
+    const options = { month: 'numeric', day: 'numeric', year: 'numeric' };
+    return date.toLocaleDateString(undefined, options);
+}
