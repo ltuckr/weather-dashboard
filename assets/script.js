0bf17d060e064a6d7989912b68d2236903194d53
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-09-12 15:42:52 -0400

    added variables

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,17 @@
 //my API key for openweathermap.org
 var myApiKey = '496d9caed19b5898e62a2e3928f9c3d3';
 
+var searchCityInput = document.getElementById('search-city');
+var searchButton = document.getElementById('search-button');
+var weatherIcon = document.getElementById('weather-icon');
+var locationElement = document.getElementById('location');
+var weatherDescriptionElement = document.querySelector('.desc');
+var futureWeatherElement = document.getElementById('future-weather');
+var fDate0 = document.getElementById('fDate0');
+var fImg0 = document.getElementById('fImg0');
+var fTemp0 = document.getElementById('fTemp0');
+var fHumidity0 = document.getElementById('fHumidity0');
+var fWind0 = document.getElementById('fWind0');
+var fUV0 = document.getElementById('fUV0');
+
+
